407c2d4f38ba747e8bd1334f774cab8b93e7c5f2
Sebastián Aguirre Vargas <SrsebastianAV@users.noreply.github.com>
2025-08-02 19:18:59 -0500

    v2

--- image-analitycs.py
+++ image-analitycs.py
@@ -7,33 +7,21 @@ import cv2
 import os
 from path import Gestor_img
 #
-gestor_img_1 = Gestor_img()
-print("Carpeta actual:", gestor_img_1.cwd()) #“current working directory” (CWD)
-img_folder = gestor_img_1.img_folder("erco_termo")
+# gestor_img_1 = Gestor_img("erco_termo")
+# print("Carpeta actual:", gestor_img_1.cwd()) #“current working directory” (CWD)
+# img_folder = gestor_img_1.img_folder()
+# paths = gestor_img_1.img_path()
+# print(paths)
+# gestor_img_1.plot_imgs()
 
-# path = "C:/Users/SebastiánAguirre/PycharmProjects/SeriesTiempoPrediction/image-processing/erco_termo/imagen (38).jpg"
-# print(path)
-# img = cv2.imread(path)
-# print(img)
-# cv2.imshow("", img)
-# cv2.waitKey(0)
+path = r"C:/Users/SebastiánAguirre/PycharmProjects/SeriesTiempoPrediction/image-processing/erco_termo/imagen.jpg"
+print(path)
+img = cv2.imread(path)
+print(img)
+cv2.imshow("", img)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
-#
-# carpeta_base_s = r"C:/Users/SebastiánAguirre/PycharmProjects/SeriesTiempoPrediction/image-processing/erco_termo"
-# for i in os.listdir(carpeta_base_s):
-#     path = os.path.join(carpeta_base_s, i).replace('\\', '/')
-#     print(path)
-#     print("Existe?", os.path.exists(path))
-#     img = cv2.imread(path)
-#     print(img)
-#     cv2.imshow('thermo', img)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
 
-# for i in range(10):
-#     img = tiff.imread(dfs["ruta"].iloc[i])
-#     print(img.shape)
-#     print(img)
 
 
 
--- path.py
+++ path.py
@@ -3,23 +3,39 @@
 """
 import os
 
+import cv2
+
 
 class Gestor_img:
-    def __init__(self):
-        pass
-    def cwd(self):
-        """
-        Get current working directory CWD
-        :return: CWD
-        """
-        return os.getcwd()
-    def img_folder(self, img_folder_name):
-        """
+    def __init__(self, img_folder_name):
+        self.img_folder_name = os.path.join(img_folder_name)
+    # def cwd(self):
+    #     """
+    #     Get current working directory CWD
+    #     :return: CWD
+    #     """
+    #     return os.getcwd()
+    # def img_folder(self):
+    #     """
+    #
+    #     :param img_folder_name: Name of the image folder saved on the same os.path.dirname(__file__)
+    #     :return: path with image folder
+    #     """
+    #     return os.path.join(os.path.dirname(__file__), self.img_folder_name)
 
-        :param img_folder_name: Name of the image folder saved on the same os.path.dirname(__file__)
-        :return: path with image folder
+    def img_path(self):
         """
-        return os.path.join(os.path.dirname(__file__), img_folder_name)
+        List comprehension used to get all the image filenames in a folder (coming of function: img_folder)
+        :return: names
+        """
+        img_path = [os. path.join(self.img_folder(), n) for n in os.listdir(self.img_folder())]
+        return img_path
+    def plot_imgs(self):
+        for p in self.img_path():
+            img = cv2.imread(p)
+            if img is not None:
+                cv2.imshow(f'Image{p}', img)
+            else:
+                print(f'Could not read image {p}')
 
-    def img_names(self):
-        pass
+
